c56cb5d897bfb503bc26cb03aea909e9576709b3
Chris Oloff <christian.oloff@gmail.com>
2017-01-20 15:06:48 +0200

    webpack config, next attempt

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const webpack = require("webpack"),
     execSync = require('child_process').execSync
 
 const pkg = require('./package.json')
-const isProd = process.env.NODE_ENV == 'production' ? true : false
 
 let plugins = [
   new webpack.DefinePlugin({
@@ -13,64 +12,32 @@ let plugins = [
   }),
 ]
 
-if (isProd) {
-  plugins = plugins.concat([
-    new webpack.optimize.AggressiveMergingPlugin({}),
-    new webpack.optimize.OccurenceOrderPlugin(true),
-    new webpack.optimize.UglifyJsPlugin({
-        compress: {
-            drop_console: true,
-            screw_ie8: true,
-            sequences: true,
-            properties: true,
-            dead_code: true,
-            drop_debugger: true,
-            conditionals: true,
-            comparisons: true,
-            evaluate: true,
-            booleans: true,
-            loops: true,
-            unused: true,
-            if_return: true,
-            join_vars: true,
-            cascade: true,
-            negate_iife: true,
-            hoist_funs: true,
-            warnings: false,
-        },
-        mangle: {
-            screw_ie8: true,
-        },
-        output: {
-            screw_ie8: true,
-            preamble: '/* Uber5 Validator South Africa - ' + new Date() + ' */',
-        },
-    }),
-  ])
-}
-
 module.exports = [
-  // client (browser)
   {
-    entry: {
-  	  app: './src/index.js',
-    },
+    entry: './src/index.js',
     output: {
-      path: './',
-      filename: 'index.js',
+      path: './lib',
+      filename: pkg.name + '.js',
+      library: pkg.name,
+      libraryTarget: 'umd',
+      umdNamedDefine: true
     },
     plugins: plugins,
-    devtool: '#source-map',
     module: {
       loaders: [
         {
           test: /.js?$/,
-          loader: 'babel-loader',
+          loader: 'babel',
           exclude: /node_modules/,
           query: {
-            presets: [ 'es2015', 'react', 'stage-2' ]
+            presets: [ 'es2015', 'stage-2' ]
           }
         },
+        {
+          test: /.js?$/,
+          loader: 'eslint',
+          exclude: /node_modules/
+        }
       ]
     },
   },
